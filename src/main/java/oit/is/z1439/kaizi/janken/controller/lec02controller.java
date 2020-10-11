bc95936c305f44e78bd30763c6148507b45bef46
e1b18075 <e1b18075@oit.ac.jp>
2020-10-11 21:28:42 +0900

    Hi ???? がindex.htmlで入力したユーザー名が表示される

---
+++ src/main/java/oit/is/z1439/kaizi/janken/controller/lec02controller.java
@@ -0,0 +1,27 @@
+package oit.is.z1439.kaizi.janken.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class lec02controller {
+
+  /**
+   * lec02というGETリクエストがあったら，lec02()を呼び出して，lec02.htmlを返すメソッド
+   *
+   * @return
+   */
+  @GetMapping("/lec02")
+  public String lec02() {
+    return "lec02.html";
+  }
+
+  @PostMapping("/lec02")
+  public String lec02(@RequestParam String username, ModelMap model) {
+    model.addAttribute("username", username);
+    return "lec02.html";
+  }
+}
